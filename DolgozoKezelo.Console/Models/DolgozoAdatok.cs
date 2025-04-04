e4792fe082929b31234d5a0ad6159f827083a909
szt3molcsa <szt3molcsa@vasvari.org>
2025-04-04 12:22:57 +0200

    e

---
+++ DolgozoKezelo.Console/Models/DolgozoAdatok.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DolgozoKezelo.Console.Models
+{
+    class DolgozoAdatok
+    {
+        private string _email;
+        private decimal _salary;
+        public string Name { get; set; }
+        public string Email { get => _email; set =>_email = value; }
+        public decimal Salary { get => _salary; set => _salary = value; }
+
+        public DolgozoAdatok(string name, string email)
+        {
+            if (string.IsNullOrWhiteSpace(name) || string.IsNullOrWhiteSpace(email)) throw new InvalidDataException("Név és Email nem lehet üres!");
+            if (!email.Contains('@')) throw new InvalidDataException("Helytelen Email cím!");
+            Name = name;
+            _email = email;
+            _salary = 0;
+        }
+    }
+}
